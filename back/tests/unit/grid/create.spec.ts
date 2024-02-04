c2692962bec79ead2c18fe36883bf5d71b65b262
Clement Beaurain <clementbeaurainpro@gmail.com>
2024-02-04 17:52:26 +0100

    test(): grid create test ok

--- back/tests/unit/grid/create.spec.ts
+++ back/tests/unit/grid/create.spec.ts
@@ -1,6 +1,15 @@
+import Grid from '#models/grid'
+import Piece from '#models/piece'
 import { test } from '@japa/runner'
 
-test.group('Grid create', () => {
-  test('example test', async ({ assert }) => {
-  })
+test('Grid is properly constructed', async ({ assert }) => {
+  let pieceList = []
+  for (let i = 0; i < 5; i++) {
+    pieceList.push(new Piece(1, 0, 0, 'line'))
+  }
+  const grid = new Grid(pieceList)
+  assert.equal(grid.width, 10)
+  assert.equal(grid.height, 20)
+  assert.deepEqual(pieceList, grid.piecesList)
+  assert.deepEqual(grid.currentPiece, undefined)
 })
