e5a926ebf572275caa7588c52ef2879f4f07d895
Clement Beaurain <clementbeaurainpro@gmail.com>
2024-02-07 22:08:20 +0100

    feat(): react router setup ok

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store/store.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './store/store.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.tsx';
+import GamePage from './pages/gamePage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/lobby',
+    element: <GamePage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
