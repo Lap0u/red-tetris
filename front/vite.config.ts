fe1f908c819223bc7b37ba8d3595bbf46599ebf5
Clement Beaurain <clementbeaurainpro@gmail.com>
2024-02-18 23:45:18 +0100

    fix(): vitest ignore config files

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
+
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    exclude: [
+      ...configDefaults.exclude,
+      '**/**/**postcss.config.js',
+      '**/**/**tailwind.config.js ',
+    ],
+  },
+});
